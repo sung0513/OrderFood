2e23dcbdcf114decdffba8ff581016f0eb478eb1
sung0513 <rlatjddls367@naver.com>
2020-01-20 13:35:18 +0900

    3번째

--- src/main/java/springsecurity/springsecurityjwt/config/SecurityConfig.java
+++ src/main/java/springsecurity/springsecurityjwt/config/SecurityConfig.java
@@ -7,22 +7,25 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.websocket.OnClose;
+
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
-        auth.inMemoryAuthentication().withUser("sung").password("1234").roles("admin");
+        auth.inMemoryAuthentication().withUser("sung").password("1234");
 
     }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
             http
                 .authorizeRequests()
                                 .antMatchers("/admin/**").hasRole("ADMIN")
-                                .antMatchers("/**").permitAll();
+                                .antMatchers("/sung/**").permitAll();
                 http.formLogin();
-
     }
 }
 
